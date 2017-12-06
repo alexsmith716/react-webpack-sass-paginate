1b5a00909c349aa642437122ca49db305f863c95
Alex Smith <alexsmith716@outlook.com>
2017-12-06 15:30:03 -0500

    Many changes. Webpack is building error free.

--- loaders/sassLoader.js
+++ loaders/sassLoader.js
@@ -4,10 +4,10 @@ console.log('>>>>>>>> sassLoader.js <<<<<<<<<<<');
 
 module.exports = (data, file) => {
 
-    try {
-        return sass.renderSync({data, file}).css.toString('utf8');
-    } catch (e) {
-        console.error(e);
-    }
+  try {
+      return sass.renderSync({data, file}).css.toString('utf8');
+  } catch (e) {
+      console.error(e);
+  }
 
 };
